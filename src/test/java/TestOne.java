effe81e00742d8b856591ec700689fb9fefd8abd
Michal <michal.tyburski1@gmail.com>
2025-10-15 00:52:47 +0200

    Added header api key

--- src/test/java/TestOne.java
+++ src/test/java/TestOne.java
@@ -1,53 +1,58 @@
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
 public class TestOne {
+
+    private static final String BASE_URL = "https://reqres.in/api";
+    private static final String API_KEY = "reqres-free-v1";
 
     @BeforeClass
     public void setup() {
-        RestAssured.baseURI = "https://reqres.in/api";
+        RestAssured.baseURI = BASE_URL;
     }
 
     @Test
     public void testGetUser() {
-        Response response = RestAssured
-                .given()
+        given()
+                .header("x-api-key", API_KEY)
                 .when()
                 .get("/users/2")
                 .then()
                 .statusCode(200)
-                .extract().response();
-
-
-        Assert.assertEquals(response.jsonPath().getString("data.email"), "janet.weaver@reqres.in");
-        Assert.assertNotNull(response.jsonPath().getString("support.text"));
+                .body("data.id", equalTo(2))
+                .body("data.email", containsString("@"));
     }
 
     @Test
     public void testCreateUser() {
-        String body = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+        String requestBody = """
+                {
+                    "name": "John Doe",
+                    "job": "Tester"
+                }
+                """;
 
-        Response response = RestAssured
-                .given()
-                .header("Content-Type", "application/json")
-                .body(body)
+        given()
+                .header("x-api-key", API_KEY)
+                .contentType("application/json")
+                .body(requestBody)
                 .when()
                 .post("/users")
                 .then()
                 .statusCode(201)
-                .extract().response();
-
-        Assert.assertNotNull(response.jsonPath().getString("id"));
-        Assert.assertNotNull(response.jsonPath().getString("createdAt"));
+                .body("name", equalTo("John Doe"))
+                .body("job", equalTo("Tester"));
     }
 
     @Test
     public void testDeleteUser() {
-        RestAssured
-                .given()
+        given()
+                .header("x-api-key", API_KEY)
                 .when()
                 .delete("/users/2")
                 .then()
